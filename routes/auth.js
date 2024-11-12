d534525f07b7d96742661be1818f4084d48ed41f
Alujan28 <kovialujan@gmail.com>
2024-11-13 01:05:29 +0530

    feat:updates in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = mongoose.model("User");
+const bcrypt = require('bcrypt')
 
-router.get('/', (req, res) => {
-    res.send("Hello");
-});
 
 router.post('/signup', (req, res) => {
     const { name, email, password } = req.body;
@@ -18,20 +16,23 @@ router.post('/signup', (req, res) => {
         if (existingUser) {
             return res.status(422).json({ error: "User already exists with that email" });
         }
-        
-        const user = new User({
-            name,
-            email,
-            password
-        });
-        
-        user.save()
-        .then((user) => {
-            res.json({ message: "Saved successfully" });
+        bcrypt.hash(password,12)
+        .then(hashedpassword=>{
+            const user = new User({
+                email,
+                password:hashedpassword,
+                name
+            });
+            
+            user.save()
+            .then((user) => {
+                res.json({ message: "Saved successfully" });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
         })
-        .catch((err) => {
-            console.log(err);
-        });
+      
     })
     .catch((err) => {
         console.log(err);
