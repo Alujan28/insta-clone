3aa5621325041e7a6c3ed56965907e64f88e7250
Alujan28 <kovialujan@gmail.com>
2024-11-15 14:17:31 +0530

    feat: add backend requireLogin.js

--- keys.js
+++ keys.js
@@ -1,3 +1,4 @@
 module.exports={
-    MONGOURI:"mongodb+srv://alujan:HdkhqcY3LewpAaPf@cluster0.5wceq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    MONGOURI:"mongodb+srv://alujan:HdkhqcY3LewpAaPf@cluster0.5wceq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    JWT_SECRET:"DNKJDBJHDFJNC11"
 }
